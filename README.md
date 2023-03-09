342af5e180e917ede4ca807a254c0b0d8d84ec3a
ngochuyduong <huy.duong@saigontechnology.com>
2023-03-09 22:21:01 +0700

    feat: source and storybook

---
+++ README.md
@@ -0,0 +1,31 @@
+# rn-base-component
+
+React native base component
+
+## Installation
+
+```sh
+npm install rn-base-component
+```
+
+## Usage
+
+```js
+import { RnBaseComponentView } from "rn-base-component";
+
+// ...
+
+<RnBaseComponentView color="tomato" />
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ example/src/App.tsx
@@ -0,0 +1,29 @@
+// import * as React from 'react';
+
+// import { StyleSheet, View } from 'react-native';
+// import { RnBaseComponentView, RnBaseButton } from 'rn-base-component';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <RnBaseComponentView color="#32a852" style={styles.box} />
+//       <RnBaseButton />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   box: {
+//     width: 60,
+//     height: 60,
+//     marginVertical: 20,
+//   },
+// });
+
+import StorybookUIRoot from '../.ondevice/Storybook';
+export { StorybookUIRoot as default };
